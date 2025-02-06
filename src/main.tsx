7f4faadc3629288868620308e2b52d599b1cf9d7
t0b1-code <rak4chris@gmail.com>
2025-02-06 23:06:32 +0300

    linked to dist master, put dark mode, set title and icon

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initalColorMode={theme.config.initalColorMode} />
         <App />
       </ChakraProvider>
       <ReactQueryDevtools />
